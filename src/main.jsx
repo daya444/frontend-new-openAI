ea4cf147296a05fe7a402e071081f6e5fe2f1f5a
daya444 <dayavelusamy@gmail.com>
2025-05-12 14:23:44 +0530

    from new branch

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import axios from "axios";
-axios.defaults.baseURL = "https://backend-gqib.onrender.com/api/v1";
+axios.defaults.baseURL = "http://localhost:5001/api/v1";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
